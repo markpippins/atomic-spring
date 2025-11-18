cc17a706ef92be4784f1cc984ab50903e22e9b33
markpippins <mpippins@gmail.com>
2025-11-18 07:15:33 -0500

    added tests

--- file-service/src/test/java/com/angrysurfer/atomic/fs/RestFsServiceTest.java
+++ file-service/src/test/java/com/angrysurfer/atomic/fs/RestFsServiceTest.java
@@ -42,8 +42,9 @@ class RestFsServiceTest {
         String token = "test-token";
         List<String> path = Arrays.asList("home", "user");
         Map<String, Object> mockResponse = Map.of("items", Arrays.asList(Map.of("name", "file1.txt", "type", "file")));
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(ServiceResponse.ok(mockResponse, "test-id"));
-        when(restFsClient.listFiles(anyString(), anyList())).thenReturn(mockResponse);
+        ServiceResponse<?> serviceResponse = ServiceResponse.ok(mockResponse, "test-id");
+        doReturn(serviceResponse).when(broker).submit(any(ServiceRequest.class));
+        doReturn(mockResponse).when(restFsClient).listFiles(anyString(), anyList());
 
         // Act
         var result = restFsService.listFiles(token, path);
@@ -58,7 +59,8 @@ class RestFsServiceTest {
         // Arrange
         String token = "invalid-token";
         List<String> path = Arrays.asList("home", "user");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(ServiceResponse.error(List.of(), "test-id"));
+        ServiceResponse<?> serviceResponse = ServiceResponse.error(List.of(), "test-id");
+        doReturn(serviceResponse).when(broker).submit(any(ServiceRequest.class));
 
         // Act & Assert
         RuntimeException exception = assertThrows(RuntimeException.class, 
@@ -73,8 +75,9 @@ class RestFsServiceTest {
         String token = "test-token";
         List<String> path = Arrays.asList("home", "user");
         Map<String, Object> mockResponse = Map.of("path", "home/user");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(ServiceResponse.ok(mockResponse, "test-id"));
-        when(restFsClient.changeDirectory(anyString(), anyList())).thenReturn(mockResponse);
+        ServiceResponse<?> serviceResponse = ServiceResponse.ok(mockResponse, "test-id");
+        doReturn(serviceResponse).when(broker).submit(any(ServiceRequest.class));
+        doReturn(mockResponse).when(restFsClient).changeDirectory(anyString(), anyList());
 
         // Act
         var result = restFsService.changeDirectory(token, path);
@@ -90,8 +93,9 @@ class RestFsServiceTest {
         String token = "test-token";
         List<String> path = Arrays.asList("home", "new-dir");
         Map<String, Object> mockResponse = Map.of("created", "home/new-dir");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(ServiceResponse.ok(mockResponse, "test-id"));
-        when(restFsClient.createDirectory(anyString(), anyList())).thenReturn(mockResponse);
+        ServiceResponse<?> serviceResponse = ServiceResponse.ok(mockResponse, "test-id");
+        doReturn(serviceResponse).when(broker).submit(any(ServiceRequest.class));
+        doReturn(mockResponse).when(restFsClient).createDirectory(anyString(), anyList());
 
         // Act
         var result = restFsService.createDirectory(token, path);
@@ -107,8 +111,9 @@ class RestFsServiceTest {
         String token = "test-token";
         List<String> path = Arrays.asList("home", "old-dir");
         Map<String, Object> mockResponse = Map.of("deleted", "home/old-dir");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(ServiceResponse.ok(mockResponse, "test-id"));
-        when(restFsClient.removeDirectory(anyString(), anyList())).thenReturn(mockResponse);
+        ServiceResponse<?> serviceResponse = ServiceResponse.ok(mockResponse, "test-id");
+        doReturn(serviceResponse).when(broker).submit(any(ServiceRequest.class));
+        doReturn(mockResponse).when(restFsClient).removeDirectory(anyString(), anyList());
 
         // Act
         var result = restFsService.removeDirectory(token, path);
@@ -125,8 +130,9 @@ class RestFsServiceTest {
         List<String> path = Arrays.asList("home", "user");
         String filename = "newfile.txt";
         Map<String, Object> mockResponse = Map.of("created_file", "home/user/newfile.txt");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(ServiceResponse.ok(mockResponse, "test-id"));
-        when(restFsClient.createFile(anyString(), anyList(), anyString())).thenReturn(mockResponse);
+        ServiceResponse<?> serviceResponse = ServiceResponse.ok(mockResponse, "test-id");
+        doReturn(serviceResponse).when(broker).submit(any(ServiceRequest.class));
+        doReturn(mockResponse).when(restFsClient).createFile(anyString(), anyList(), anyString());
 
         // Act
         var result = restFsService.createFile(token, path, filename);
@@ -143,8 +149,9 @@ class RestFsServiceTest {
         List<String> path = Arrays.asList("home", "user");
         String filename = "oldfile.txt";
         Map<String, Object> mockResponse = Map.of("deleted_file", "home/user/oldfile.txt");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(ServiceResponse.ok(mockResponse, "test-id"));
-        when(restFsClient.deleteFile(anyString(), anyList(), anyString())).thenReturn(mockResponse);
+        ServiceResponse<?> serviceResponse = ServiceResponse.ok(mockResponse, "test-id");
+        doReturn(serviceResponse).when(broker).submit(any(ServiceRequest.class));
+        doReturn(mockResponse).when(restFsClient).deleteFile(anyString(), anyList(), anyString());
 
         // Act
         var result = restFsService.deleteFile(token, path, filename);
@@ -161,8 +168,9 @@ class RestFsServiceTest {
         List<String> path = Arrays.asList("home", "user", "oldname.txt");
         String newName = "newname.txt";
         Map<String, Object> mockResponse = Map.of("renamed", "old", "to", "new");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(ServiceResponse.ok(mockResponse, "test-id"));
-        when(restFsClient.rename(anyString(), anyList(), anyString())).thenReturn(mockResponse);
+        ServiceResponse<?> serviceResponse = ServiceResponse.ok(mockResponse, "test-id");
+        doReturn(serviceResponse).when(broker).submit(any(ServiceRequest.class));
+        doReturn(mockResponse).when(restFsClient).rename(anyString(), anyList(), anyString());
 
         // Act
         var result = restFsService.rename(token, path, newName);
@@ -183,11 +191,11 @@ class RestFsServiceTest {
         String toAlias = "toUser";
         Map<String, Object> mockResponse = Map.of("copied", "from", "to", "to");
         
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(
-            ServiceResponse.ok(new UserRegistrationDTO() {{ setAlias(fromAlias); }}, "test-id"),
-            ServiceResponse.ok(new UserRegistrationDTO() {{ setAlias(toAlias); }}, "test-id")
-        );
-        when(restFsClient.copy(anyString(), anyList(), anyString(), anyList())).thenReturn(mockResponse);
+        doReturn(ServiceResponse.ok(new UserRegistrationDTO() {{ setAlias(fromAlias); }}, "test-id"))
+            .when(broker).submit(any(ServiceRequest.class));
+        doReturn(ServiceResponse.ok(new UserRegistrationDTO() {{ setAlias(toAlias); }}, "test-id"))
+            .when(broker).submit(any(ServiceRequest.class));
+        doReturn(mockResponse).when(restFsClient).copy(anyString(), anyList(), anyString(), anyList());
 
         // Act
         var result = restFsService.copy(fromToken, fromPath, toToken, toPath);
@@ -204,8 +212,9 @@ class RestFsServiceTest {
         List<String> path = Arrays.asList("home", "user");
         String filename = "test.txt";
         Map<String, Object> mockResponse = Map.of("exists", true, "type", "file");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(ServiceResponse.ok(mockResponse, "test-id"));
-        when(restFsClient.hasFile(anyString(), anyList(), anyString())).thenReturn(mockResponse);
+        ServiceResponse<?> serviceResponse = ServiceResponse.ok(mockResponse, "test-id");
+        doReturn(serviceResponse).when(broker).submit(any(ServiceRequest.class));
+        doReturn(mockResponse).when(restFsClient).hasFile(anyString(), anyList(), anyString());
 
         // Act
         var result = restFsService.hasFile(token, path, filename);
@@ -222,8 +231,9 @@ class RestFsServiceTest {
         List<String> path = Arrays.asList("home", "user");
         String folderName = "testFolder";
         Map<String, Object> mockResponse = Map.of("exists", true, "type", "directory");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(ServiceResponse.ok(mockResponse, "test-id"));
-        when(restFsClient.hasFolder(anyString(), anyList(), anyString())).thenReturn(mockResponse);
+        ServiceResponse<?> serviceResponse = ServiceResponse.ok(mockResponse, "test-id");
+        doReturn(serviceResponse).when(broker).submit(any(ServiceRequest.class));
+        doReturn(mockResponse).when(restFsClient).hasFolder(anyString(), anyList(), anyString());
 
         // Act
         var result = restFsService.hasFolder(token, path, folderName);
@@ -243,8 +253,9 @@ class RestFsServiceTest {
             Map.of("name", "def.json", "type", "file")
         );
         Map<String, Object> mockResponse = Map.of("moved", "items");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(ServiceResponse.ok(mockResponse, "test-id"));
-        when(restFsClient.moveItems(anyString(), anyList(), anyList(), anyList())).thenReturn(mockResponse);
+        ServiceResponse<?> serviceResponse = ServiceResponse.ok(mockResponse, "test-id");
+        doReturn(serviceResponse).when(broker).submit(any(ServiceRequest.class));
+        doReturn(mockResponse).when(restFsClient).moveItems(anyString(), anyList(), anyList(), anyList());
 
         // Act
         var result = restFsService.moveItems(token, sourcePath, destPath, items);
@@ -262,7 +273,7 @@ class RestFsServiceTest {
         UserRegistrationDTO userRegistration = new UserRegistrationDTO();
         userRegistration.setAlias(expectedAlias);
         ServiceResponse<UserRegistrationDTO> response = ServiceResponse.ok(userRegistration, "test-id");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(response);
+        doReturn(response).when(broker).submit(any(ServiceRequest.class));
 
         // Use reflection or test the private method indirectly
         String resultAlias = null;
@@ -284,7 +295,7 @@ class RestFsServiceTest {
         // Arrange
         String token = "invalid-token";
         ServiceResponse<UserRegistrationDTO> response = ServiceResponse.error(List.of(), "test-id");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(response);
+        doReturn(response).when(broker).submit(any(ServiceRequest.class));
 
         // Use reflection to test the private method
         String resultAlias = null;
@@ -310,10 +321,10 @@ class RestFsServiceTest {
         UserRegistrationDTO userRegistration = new UserRegistrationDTO();
         userRegistration.setAlias("testUser");
         ServiceResponse<UserRegistrationDTO> response = ServiceResponse.ok(userRegistration, "test-id");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(response);
+        doReturn(response).when(broker).submit(any(ServiceRequest.class));
         
         Map<String, Object> mockResponse = Map.of("items", Arrays.asList(Map.of("name", "test.txt", "type", "file")));
-        when(restFsClient.listFiles(anyString(), anyList())).thenReturn(mockResponse);
+        doReturn(mockResponse).when(restFsClient).listFiles(anyString(), anyList());
 
         // Act
         var result = restFsService.listFiles(token, path);
@@ -340,8 +351,8 @@ class RestFsServiceTest {
         UserRegistrationDTO userRegistration = new UserRegistrationDTO();
         userRegistration.setAlias("testUser");
         ServiceResponse<UserRegistrationDTO> response = ServiceResponse.ok(userRegistration, "test-id");
-        when(broker.submit(any(ServiceRequest.class))).thenReturn(response);
-        when(restFsClient.listFiles(anyString(), anyList())).thenReturn(mockResponse);
+        doReturn(response).when(broker).submit(any(ServiceRequest.class));
+        doReturn(mockResponse).when(restFsClient).listFiles(anyString(), anyList());
 
         // Act
         var result = restFsService.listFiles(token, null);
