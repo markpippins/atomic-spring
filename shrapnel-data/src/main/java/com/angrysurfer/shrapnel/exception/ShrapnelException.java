b773690315999162c332739bebf2f272440b1deb
mpippins <mpippins@gmail.com>
2026-01-08 05:18:33 +0000

    diet flavor

--- shrapnel-data/src/main/java/com/angrysurfer/shrapnel/exception/ShrapnelException.java
+++
@@ -1,7 +0,0 @@
-package com.angrysurfer.shrapnel.exception;
-
-public class ShrapnelException extends RuntimeException {
-    public ShrapnelException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- shrapnel-data/src/main/java/com/angrysurfer/shrapnel/filter/DataFilters.java
+++
@@ -1,7 +0,0 @@
-package com.angrysurfer.shrapnel.filter;
-
-import java.util.ArrayList;
-
-// this is the best class I've written in my entire career.
-public class DataFilters extends ArrayList<IDataFilter> implements IDataFilters {
-}
