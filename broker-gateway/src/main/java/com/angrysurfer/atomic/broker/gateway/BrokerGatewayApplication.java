50f7741ccfa13775743b65ff9e9abd5fe700fc86
markpippins <mpippins@gmail.com>
2026-01-23 04:26:37 -0500

    sql backend

--- broker-gateway/src/main/java/com/angrysurfer/atomic/broker/gateway/BrokerGatewayApplication.java
+++ broker-gateway/src/main/java/com/angrysurfer/atomic/broker/gateway/BrokerGatewayApplication.java
@@ -4,31 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = {
-    "com.angrysurfer.atomic.broker",
-    "com.angrysurfer.atomic.user",
-    "com.angrysurfer.atomic.fs",
-    "com.angrysurfer.atomic.login",
-    "com.angrysurfer.atomic.note",
-    "com.angrysurfer.atomic.search",
-    "com.angrysurfer.atomic.registry",
-    "com.angrysurfer.atomic.admin.logging"
-})
-@EnableJpaRepositories(basePackages = {
-    "com.angrysurfer.atomic.broker",
-    "com.angrysurfer.atomic.user",
-    "com.angrysurfer.atomic.fs",
-    "com.angrysurfer.atomic.login",
-    "com.angrysurfer.atomic.note",
-    "com.angrysurfer.atomic.search",
-    "com.angrysurfer.atomic.registry",
-    "com.angrysurfer.atomic.admin.logging"
-})
-@EntityScan(basePackages = {
     "com.angrysurfer.atomic.broker",
     "com.angrysurfer.atomic.user",
     "com.angrysurfer.atomic.fs",
